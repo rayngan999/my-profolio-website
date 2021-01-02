ab66ed416e5171c775312600c3335ae19215e13e
rayngan999 <rayngan999@gmail.com>
2021-01-02 01:25:41 -0800

    u

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,21 @@ const config = require('./config/site');
 
 module.exports = {
   siteMetadata: {
-    title: "Ray Ngan", // Navigation and Site Title
+    pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  title: "Ray Ngan", // Navigation and Site Title
   titleAlt: 'Ray Ngan', // Title for JSONLD
   description: "Ray Ngan's Portfolio Website",
-  url: 'https://rayngan.gtsb.io/', // Doma,
-  favicon: 'static/logo/favicon.png'
+  url: 'https://rayngan.gtsb.io/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://rayngan.gtsb.io/', // url + pathPrefix
+  siteLanguage: 'en', // Language Tag on <html> element
+  logo: 'static/logo/logo.png', // Used for SEO
+  banner: 'static/logo/banner.png',
+  // JSONLD / Manifest
+  favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
+  shortName: 'Ray', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Ray', // Author for schemaORGJSONLD
+  themeColor: '#3e7bf2',
+  backgroundColor: '#d3e0ff'
   },
   plugins: [
     {
