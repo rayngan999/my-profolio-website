350811d2b0c9d50a2ee3cdcd1db5ff1ef29d2cdf
rayngan999 <rayngan999@gmail.com>
2021-03-25 16:46:15 -0700

    u

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,103 +1,95 @@
-// const path = require('path');
+const path = require('path');
 
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions;
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
 
-//   return new Promise((resolve, reject) => {
-//     // const postTemplate = path.resolve('src/templates/post.jsx');
-//     // const tagPage = path.resolve('src/pages/tags.jsx');
-//     // const tagPosts = path.resolve('src/templates/tag.jsx');
+  return new Promise((resolve, reject) => {
+    // const postTemplate = path.resolve('src/templates/post.jsx');
+    // const tagPage = path.resolve('src/pages/tags.jsx');
+    // const tagPosts = path.resolve('src/templates/tag.jsx');
 
-//     resolve(
-//       graphql(
-//         `
-//           query {
-//             allMarkdownRemark(
-//               sort: { order: ASC, fields: [frontmatter___date] }
-//             ) {
-//               edges {
-//                 node {
-//                   frontmatter {
-//                     path
-//                     title
-//                     tags
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         `
-//       ).then(result => {
-//         if (result.errors) {
-//           return reject(result.errors);
-//         }
+    resolve(
+      graphql(
+        `
+          query {
+            allMarkdownRemark(
+              sort: { order: ASC, fields: [frontmatter___date] }
+            ) {
+              edges {
+                node {
+                  frontmatter {
+                    path
+                    title
+                  }
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          return reject(result.errors);
+        }
 
-//         const posts = result.data.allMarkdownRemark.edges;
+        // const posts = result.data.allMarkdownRemark.edges;
 
-//         const postsByTag = {};
-//         // create tags page
-//         // posts.forEach(({ node }) => {
-//         //   if (node.frontmatter.tags) {
-//         //     node.frontmatter.tags.forEach(tag => {
-//         //       if (!postsByTag[tag]) {
-//         //         postsByTag[tag] = [];
-//         //       }
 
-//         //       postsByTag[tag].push(node);
-//         //     });
-//         //   }
-//         // });
+        // // create posts
+        // posts.forEach(({ node }, index) => {
+        //   const path = node.frontmatter.path;
+        //   const prev = index === 0 ? null : posts[index - 1].node;
+        //   const next =
+        //     index === posts.length - 1 ? null : posts[index + 1].node;const path = require(`path`)
+        //     // Log out information after a build is done
+        //     exports.onPostBuild = ({ reporter }) => {
+        //       reporter.info(`Your Gatsby site has been built!`)
+        //     }
+        //     // Create blog pages dynamically
+        //     exports.createPages = async ({ graphql, actions }) => {
+        //       const { createPage } = actions
+        //       const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+        //       const result = await graphql(`
+        //         query {
+        //           allSamplePages {
+        //             edges {
+        //               node {
+        //                 slug
+        //                 title
+        //               }
+        //             }
+        //           }
+        //         }
+        //       `)
+        //       result.data.allSamplePages.edges.forEach(edge => {
+        //         createPage({
+        //           path: `${edge.node.slug}`,
+        //           component: blogPostTemplate,
+        //           context: {
+        //             title: edge.node.title,
+        //           },
+        //         })
+        //       })
+        //     }
+        //   createPage({
+        //     path,
+        //     component: postTemplate,
+        //     context: {
+        //       pathSlug: path,
+        //       prev,
+        //       next,
+        //     },
+        //   });
+        // });
+      })
+    );
+  });
+};
 
-//         // const tags = Object.keys(postsByTag);
-
-//         // createPage({
-//         //   path: '/tags',
-//         //   component: tagPage,
-//         //   context: {
-//         //     tags: tags.sort(),
-//         //   },
-//         // });
-
-//         //create tags
-//         // tags.forEach(tagName => {
-//         //   const posts = postsByTag[tagName];
-
-//         //   createPage({
-//         //     path: `/tags/${tagName}`,
-//         //     component: tagPosts,
-//         //     context: {
-//         //       posts,
-//         //       tagName,
-//         //     },
-//         //   });
-//         // });
-
-//         //create posts
-//         // posts.forEach(({ node }, index) => {
-//         //   const path = node.frontmatter.path;
-//         //   const prev = index === 0 ? null : posts[index - 1].node;
-//         //   const next =
-//         //     index === posts.length - 1 ? null : posts[index + 1].node;
-//         //   createPage({
-//         //     path,
-//         //     component: postTemplate,
-//         //     context: {
-//         //       pathSlug: path,
-//         //       prev,
-//         //       next,
-//         //     },
-//         //   });
-//         // });
-//       })
-//     );
-//   });
-// };
-
-// /* Allows named imports */
-// exports.onCreateWebpackConfig = ({ actions }) => {
-//   actions.setWebpackConfig({
-//     resolve: {
-//       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-//     },
-//   });
-// };
+/* Allows named imports */
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  });
+};
