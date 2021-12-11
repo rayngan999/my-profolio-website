58646b7d2b03dc9046c207172e0b4b74a2e92852
rayngan999 <rayngan999@gmail.com>
2021-12-11 00:12:35 -0800

    update resume

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styled from '@emotion/styled';
 import logo from '../../static/logo/header-logo.png';
-import resume from "../Wai_Hei_Ngan_Ray_resume.pdf"
+import resume from "../Ray_Ngan_Resume.pdf"
 
 
 
